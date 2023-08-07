c972a71092f24f557e54ccfd530941061bb8226e
Matthew Schlabach <69648781+Schlabach@users.noreply.github.com>
2023-08-07 09:45:38 -0400

    Add project files.

---
+++ AppViewer/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void YouTube_Click(object sender, RoutedEventArgs e)
+        {
+            Viewer youTubeTV = new Viewer("http://tv.youtube.com/");
+            youTubeTV.Show();
+        }
+
+        private void Netflix_Click(object sender, RoutedEventArgs e)
+        {
+            Viewer netflix = new Viewer("http://www.netflix.com/");
+            netflix.Show();
+        }
+
+        private void Disney_Click(object sender, RoutedEventArgs e)
+        {
+            Viewer disney = new Viewer("http://www.disneyplus.com/");
+            disney.Show();
+        }
+
+        private void Prime_Click(object sender, RoutedEventArgs e)
+        {
+            Viewer prime = new Viewer("http://www.amazon.com/primevideo/");
+            prime.Show();
+        }
+
+        private void Starz_Click(object sender, RoutedEventArgs e)
+        {
+            Viewer starz = new Viewer("http://www.starz.com/");
+            starz.Show();
+        }
+
+        private void Peacock_Click(object sender, RoutedEventArgs e)
+        {
+            Viewer peacock = new Viewer("http://www.peacocktv.com/");
+            peacock.Show();
+        }
+    }
+}
+
+
+
+
---
+++ AppViewer/Viewer.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace AppViewer
+{
+    /// <summary>
+    /// Interaction logic for Viewer.xaml
+    /// </summary>
+    public partial class Viewer : Window
+    {
+        public Viewer(string url)
+        {
+            InitializeComponent();
+            webView.Source = new Uri(url);
+            Loaded += Viewer_Loaded;
+        }
+
+    private void Viewer_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Get the screen dimensions
+        double screenWidth = SystemParameters.PrimaryScreenWidth;
+        double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+        // Set the window position to bottom left corner
+        Left = 0;
+        Top = screenHeight - Height;
+            Topmost = true;
+    }
+}
+}
